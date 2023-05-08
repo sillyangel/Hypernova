cfa6818db84eba45449eb6e36aafae45e2214071
sillyangel <sillyangel3@gmail.com>
2023-05-08 12:17:12 +0000

    update!

---
+++ static/public/pass.js
@@ -0,0 +1,10 @@
+var password = prompt("Password of the Week", "huh?");
+var bootedoff = "https://google.com/"
+checkpassword(password);
+function checkpassword(passcode) {
+		if (passcode == "Ygsin") {
+
+  } else {
+    window.location.href = bootedoff;
+  }
+}
