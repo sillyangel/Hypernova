a6c9ae0419e71de212956f586bdb67b1be0a7463
sillyangel <sillyangel3@gmail.com>
2023-05-08 23:25:42 +0000

    cookies

--- static/public/pass.js
+++ static/public/pass.js
@@ -1,10 +1,25 @@
 var password = prompt("Password of the Week", "huh?");
 var bootedoff = "https://google.com/"
-checkpassword(password);
-function checkpassword(passcode) {
-		if (passcode == "Ygsin") {
+var cookieName = "myCookie";
+var cookieValue = "true";
+var cookieExpiration = new Date();
+cookieExpiration.setDate(cookieExpiration.getDate() + 7); // Expires in 1 week
 
+if (document.cookie.indexOf(cookieName) == -1) {
+  // Cookie doesn't exist, prompt for password
+  var password = prompt("Password of the Week", "huh?");
+  checkpassword(password);
+} else {
+  // Cookie exists, don't prompt for password
+  console.log("User already authenticated");
+}
+
+function checkpassword(passcode) {
+  if (passcode == "Ygsin") {
+    // Set cookie and redirect to success page
+    document.cookie = cookieName + "=" + cookieValue + ";expires=" + cookieExpiration.toUTCString();
   } else {
+    // Redirect to error page
     window.location.href = bootedoff;
   }
 }
